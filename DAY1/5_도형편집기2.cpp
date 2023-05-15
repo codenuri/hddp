ddf866682470e6db5208f635b250a03b45c8dafd
ecourse <smkang@codenuri.co.kr>
2023-05-15 13:39:19 +0900

    Add files via upload

--- DAY1/5_도형편집기2.cpp
+++ DAY1/5_도형편집기2.cpp
@@ -27,15 +27,19 @@ int main()
 		int cmd;
 		std::cin >> cmd;
 
-		if (cmd == 1) v.push_back(new Rect);
+		if      (cmd == 1) v.push_back(new Rect);
 		else if (cmd == 2) v.push_back(new Circle);
 		else if (cmd == 9)
 		{
 			for (auto p : v)
-				p->draw();
+				p->draw(); // error
+						// p는 Shape* 인데
+						// Shape 에는 draw 없다
 		}
 	}
 }
 // 왜? 위 코드가 에러 일까요 ?
 // 해결책은 뭘까요 ?
+// 1. Shape* 인 p를 Rect* 또는 Circle* 캐스팅
+// 2. Shape 안에도 draw()를 만들자.
 
