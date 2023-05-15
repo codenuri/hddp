3435a553215327ad165cb2ab58d494bde6ac7178
ecourse <smkang@codenuri.co.kr>
2023-05-15 11:21:23 +0900

    Add files via upload

--- DAY1/3_가상함수1-1.cpp
+++ DAY1/3_가상함수1-1.cpp
@@ -1,7 +1,11 @@
 class Animal
 {
 public:
+	// non-virtual : 포인터 타입으로 함수 호출을 결정. 빠르다.
 	void cry1() {}
+	
+	// virtual : 포인터 타입이 아닌 메모리 조사후 함수를 결정
+	//			느리다.
 	virtual void cry2() {}
 };
 class Dog : public Animal
@@ -14,6 +18,6 @@ int main()
 {
 	Dog dog;
 	Animal* p = &dog;
-	p->cry1();
+	p->cry1(); // call Animal::cry1
 	p->cry2();
 }
