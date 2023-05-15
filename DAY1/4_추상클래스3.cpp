d7ced1cd8523dbb8b62bdab185bf7d3a505942dd
ecourse <smkang@codenuri.co.kr>
2023-05-15 13:09:44 +0900

    Add files via upload

--- DAY1/4_추상클래스3.cpp
+++ DAY1/4_추상클래스3.cpp
@@ -2,26 +2,38 @@
 
 
 
+// 모든 카메라가 지켜야 하는 규칙을 먼저 설계 합니다.
+// 규칙 : 모든 카메라는 아래 클래스로 부터 파생되어야 한다
+class ICamera
+{
+public:
+	virtual void Take() = 0;
+};
 
+// 실제 카메라가 없어도 사용자는 규칙 대로만 사용하면 된다.
+class People
+{
+public:
+	void UseCamera(ICamera* p) { p->Take(); }
+};
 
-
-class Camera
+// 이제 모든 카메라는 규칙을 지켜야 한다.
+class Camera : public ICamera
 {
 public:
 	void Take() { std::cout << "Take Picture" << std::endl; }
 };
 
-class HDCamera
+class HDCamera : public ICamera
 {
 public:
 	void Take() { std::cout << "Take HD Picture" << std::endl; }
 };
 
-class People
+class UHDCamera : public ICamera
 {
 public:
-	void UseCamera(Camera* p) { p->Take(); }
-	void UseCamera(HDCamera* p) { p->Take(); }
+	void Take() { std::cout << "Take UHD Picture" << std::endl; }
 };
 
 int main()
@@ -32,6 +44,9 @@ int main()
 
 	HDCamera hc;
 	p.UseCamera(&hc);
+
+	UHDCamera uhc;
+	p.UseCamera(&uhc);
 }
 
 
