4d86605086cf1297056f3c653c0027f76c9a4ee4
ecourse <smkang@codenuri.co.kr>
2023-05-15 13:48:08 +0900

    Add files via upload

--- DAY1/5_도형편집기3.cpp
+++ DAY1/5_도형편집기3.cpp
@@ -5,13 +5,29 @@
 //		  반드시 기반 클래스에 있어야 한다.(문법이 아닌 디자인 규칙)
 //	      그래야, Shape* 로 묶었을때 해당 기능을 사용할수 있다.
 
+// 핵심 4. 기반 클래스가 가진 멤버 함수중. 파생 클래스가 override
+//        하게되는 것은 반드시 가상함수로 해야 한다.
+
 class Shape
 {
+	int color;
 public:
-	void draw() { std::cout << "draw shape" << std::endl; }
+
+	// 아래 함수는 가상으로 할까요 ? 하지말까요 ?
+	// => 아래 함수를 파생 클래스가 재정의 할 이유 없습니다.
+	// => non-virtual
+	void set_color(int c) { color = c; }
+
+	// => 모든 도형은 면적 구하는 방법이 다릅니다.
+	// => virtual 이어야 합니다.
+	virtual int get_area() { return -1;}
+
+	virtual void draw() { std::cout << "draw shape" << std::endl; }
 
 	virtual ~Shape() {}
 };
+
+
 
 class Rect : public Shape
 {
