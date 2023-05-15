b21b9cbb117805d940cb282dc5a10736acee4690
ecourse <smkang@codenuri.co.kr>
2023-05-15 14:41:45 +0900

    Add files via upload

--- DAY1/5_도형편집기6.cpp
+++ DAY1/5_도형편집기6.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <vector>
 
+// 핵심 9. template method 패턴
+// => 기반 클래스에 전체적인 흐름을 담은 메소드 제공
+// => 변하는 것만 가상함수화 해서 파생 클래스가 재정의할수
+//    있는 기회 제공
+
+// => 가정 널리 사용되는 패턴
 
 class Shape
 {
@@ -16,11 +22,13 @@ public:
 	//    분리되어야 합니다.
 	// => 변하는 것을 가상함수로 분리하세요
 
+protected:
 	virtual void draw_imp()
 	{
 		std::cout << "draw shape" << std::endl; 
 	}
-	virtual void draw()
+public:
+	void draw()
 	{
 		std::cout << "mutex lock" << std::endl;
 		draw_imp();
@@ -48,7 +56,7 @@ public:
 class Rect : public Shape
 {
 public:
-	void draw() override { std::cout << "draw rect" << std::endl; }
+	void draw_imp() override { std::cout << "draw rect" << std::endl; }
 
 	Shape* clone() override
 	{
@@ -59,7 +67,7 @@ public:
 class Circle : public Shape
 {
 public:
-	void draw() override { std::cout << "draw circle" << std::endl; }
+	void draw_imp() override { std::cout << "draw circle" << std::endl; }
 
 	Shape* clone() override
 	{
