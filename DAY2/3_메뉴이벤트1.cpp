12d632e870d986f912dac2a9b707891d17157d96
ecourse <smkang@codenuri.co.kr>
2023-05-16 14:22:36 +0900

    Add files via upload

---
+++ DAY2/3_메뉴이벤트1.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+
+
+class unsupported_operation {};
+
+class BaseMenu
+{
+	std::string title;
+public:
+	BaseMenu(const std::string& title) : title(title) {}
+
+	virtual ~BaseMenu() {}
+	std::string get_title() { return title; }
+	virtual void command() = 0;
+
+	virtual void add_menu(BaseMenu*) { throw unsupported_operation(); }
+	virtual BaseMenu* get_submenu(int idx) { throw unsupported_operation(); }
+};
+
+
+
+
+
+class PopupMenu : public BaseMenu
+{
+	std::vector<BaseMenu*> v; // 핵심
+public:
+	PopupMenu(const std::string& title) : BaseMenu(title) {}
+
+	void add_menu(BaseMenu* m) { v.push_back(m); }
+
+
+
+	void command()
+	{
+		while (1)
+		{
+			system("cls");
+
+			int sz = v.size();
+			int idx = 0;
+
+			for (auto m : v)
+			{
+				std::cout << ++idx << ". " << m->get_title() << "\n";
+			}
+
+			std::cout << sz + 1 << ". 종료\n";
+
+			std::cout << "메뉴를 선택하세요 >> ";
+			int cmd;
+			std::cin >> cmd;
+
+			if (cmd == sz + 1)
+				break;
+
+			if (cmd < 1 || cmd > sz + 1)
+				continue;
+
+			v[cmd - 1]->command();
+		}
+
+	}
+	BaseMenu* get_submenu(int idx) { return v[idx]; }
+};
+//----------------------------------------------
+
+class Dialog
+{
+public:
+	void close() { std::cout << "Dialog close" << std::endl; }
+};
+
+class MenuItem : public BaseMenu
+{
+	int id;
+public:
+	MenuItem(const std::string& title, int id)
+		: BaseMenu(title), id(id) {}
+
+	void command()
+	{
+		std::cout << get_title() << " 메뉴 선택됨" << std::endl;
+
+		getchar();
+		getchar();
+	}
+};
+int main()
+{
+	MenuItem m1("닫기", 11);
+	MenuItem m2("RED", 21);
+
+	m1.command();
+}
+
+
+
