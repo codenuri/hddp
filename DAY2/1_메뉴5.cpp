95dc873ced564eab96298786e81b4f5282bf51b3
ecourse <smkang@codenuri.co.kr>
2023-05-16 13:24:06 +0900

    Add files via upload

--- DAY2/1_메뉴5.cpp
+++ DAY2/1_메뉴5.cpp
@@ -80,24 +80,33 @@ public:
 int main()
 {
 	PopupMenu* root = new PopupMenu("root");
-
 	PopupMenu* pm1 = new PopupMenu("해상도 변경");
 	PopupMenu* pm2 = new PopupMenu("색상 변경");
 
 	root->add_menu(pm1);
 	root->add_menu(pm2);
+//	pm1->add_menu(pm2);
 
 	pm1->add_menu(new MenuItem("HD", 11));
 	pm1->add_menu(new MenuItem("FHD", 12));
 	pm1->add_menu(new MenuItem("UHD", 13));
+	pm1->add_menu(new MenuItem("8K", 14));
 
 	pm2->add_menu(new MenuItem("RED",   21));
 	pm2->add_menu(new MenuItem("GREEN", 22));
-	pm2->add_menu(new MenuItem("GREEN", 23));
+	pm2->add_menu(new MenuItem("BLUE", 23));
 
 	// 이제 메뉴를 시작 하려면 ?
+	// 각 메뉴 객체의 포함관계를 그림으로 생각해 보세요.. 
+	root->command();
 }
 
+// 객체지향 언어에서 프로그램이란 ?
 
+// 1. 객체를 생성하고
+// 2. 객체간의 관계를 설정하고
+// 3. 객체간의 메세지를 주고 받는 과정이다.(서로 멤버함수를 호출하는 것)
+
+// 객체지향 프로그램에서 모든 것은 "객체"이다.
 
 
