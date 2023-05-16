26c5fbb4d205f8f084859be10cba2c7cc490ddf9
ecourse <smkang@codenuri.co.kr>
2023-05-16 10:09:32 +0900

    Add files via upload

---
+++ DAY2/0_단위전략7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+// 사용자 정의 메모리 할당기 - STL 의 대부분 컨테이너에 전달해서 사용
+// cppreference.com 접속해 보세요
+template<typename T>
+class debug_alloc
+{
+public:
+	T* allocate(std::size_t sz)
+	{
+		void* p = malloc(sizeof(T) * sz);
+		printf("allocate : %p, %d cnts\n", p, sz);
+		return static_cast<T*>(p);
+	}
+	void deallocate(T* p, std::size_t sz)
+	{
+		printf("deallocate : %p, %d cnts\n", p, sz);
+		free(p);
+	}
+};
+
+
+int main()
+{
+	std::vector<int, debug_alloc<int> > v;
+
+	std::cout << "------------" << std::endl;
+
+	v.resize(10); // 이순간 할당기의 allocate() 호출
+
+	std::cout << "------------" << std::endl;
+
+}
