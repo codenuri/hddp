afadc1391fc630ced3bc0e9698723fde333ea47e
ecourse <smkang@codenuri.co.kr>
2023-05-16 16:37:57 +0900

    Add files via upload

--- DAY2/3_메뉴이벤트6-1.cpp
+++ DAY2/3_메뉴이벤트6-1.cpp
@@ -50,14 +50,32 @@ public:
 	}
 };
 
+// 개체를 만드는 모양이 복잡하다면
+// 객체를 만드는 함수를 제공해라
+template<typename T>
+method_command<T>* cmd(void(T::* method)(), T* target)
+{
+	return new method_command<T>(method, target);
+}
+
+function_command* cmd(void(*f)())
+{
+	return new function_command(f);
+}
+
+
 
 int main()
 {
 	Dialog dlg;
 
-	ICommand* p1 = new function_command(&foo);
-	ICommand* p2 = new method_command<Dialog>(&Dialog::close,
-												&dlg);
+	// 의미 : 일반함수 주소와 멤버 함수의 주소를 동일 타입에 보관했다가실행
+	// (ICommand* 타입)
+//	ICommand* p1 = new function_command(&foo);
+//	ICommand* p2 = new method_command<Dialog>(&Dialog::close,
+//												&dlg);
+	ICommand* p1 = cmd(&foo);
+	ICommand* p2 = cmd(&Dialog::close, &dlg);
 
 	p1->execute(); // foo()
 	p2->execute(); // dlg.close()
