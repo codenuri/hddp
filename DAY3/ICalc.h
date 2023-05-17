8a7439200d1568e95151345428a45a25ba975a34
ecourse <smkang@codenuri.co.kr>
2023-05-17 14:46:05 +0900

    Add files via upload

--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -1,9 +1,18 @@
 // ICalc.h
 #pragma once
 
-struct ICalc
+
+struct IRefCount
+{
+	// 참조계수 함수도 인터페이스에 있어야 합니다.
+	virtual void AddRef() = 0;
+	virtual void Release() = 0;
+	virtual ~IRefCount() {}
+};
+struct ICalc : public IRefCount
 {
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
 	virtual ~ICalc() {}
 };
+// cl calcproxy.cpp /LD /link user32.lib gdi32.lib kernel32.lib
