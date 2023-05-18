1a2e01b315863f4bd5ca7132bb6e0bd40cb06184
ecourse <smkang@codenuri.co.kr>
2023-05-18 16:30:16 +0900

    Add files via upload

---
+++ DAY4/5_Flyweight4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <String>
+#include <vector>
+class Character
+{
+	char ch;
+
+	std::string font_name;
+	int color;
+	int size;
+public:
+};
+
+int main()
+{
+	std::vector<Character> v[10];
+}
