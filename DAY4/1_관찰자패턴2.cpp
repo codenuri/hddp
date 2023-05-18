845c636d434e41e9839afbebddee962a14120842
ecourse <smkang@codenuri.co.kr>
2023-05-18 09:21:33 +0900

    Add files via upload

---
+++ DAY4/1_관찰자패턴2.cpp
@@ -0,0 +1,84 @@
+//1_관찰자패턴1 - 94 page
+#include <iostream>
+#include <vector>
+
+
+struct IGraph
+{
+	virtual void update(int data) = 0;
+	virtual ~IGraph() {}
+};
+
+// 관찰자 패턴의 기본 코드는 변하지 않습니다.
+// 기반 클래스를 설계해서 제공합니다.
+// 관찰자 패턴에서 관찰의 대상을 "subject" 라고 합니다.
+class Subject
+{
+	std::vector<IGraph*> v;
+public:
+	void attach(IGraph* p) { v.push_back(p); }
+	void detach(IGraph* p) {}
+	void notify(int data)
+	{
+
+		for (auto p : v)
+			p->update(data);
+	}
+};
+class Table : public Subject 
+{
+	int value;
+public:
+	void edit()
+	{
+		while (1)
+		{
+			std::cout << "Data >>";
+			std::cin >> value;
+			notify(value);	
+		}
+	}
+};
+//----------------------
+class BarGraph : public IGraph
+{
+public:
+	void update(int n) override
+	{
+		std::cout << "Bar Graph : ";
+
+		for (int i = 0; i < n; i++)
+			std::cout << "*";
+
+		std::cout << std::endl;
+	}
+};
+
+class PieGraph : public IGraph
+{
+public:
+	void update(int n) override
+	{
+		std::cout << "Pie Graph : ";
+
+		for (int i = 0; i < n; i++)
+			std::cout << ")";
+
+		std::cout << std::endl;
+	}
+};
+
+
+int main()
+{
+	Table t;
+	PieGraph pg; t.attach(&pg);
+	BarGraph bg; t.attach(&bg);
+
+	t.edit();
+}
+
+
+
+
+
