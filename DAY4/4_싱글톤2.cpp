bd6a8e4a4a8c563cc63d0a0a8e491122eca2201c
ecourse <smkang@codenuri.co.kr>
2023-05-18 14:29:42 +0900

    Add files via upload

--- DAY4/4_싱글톤2.cpp
+++ DAY4/4_싱글톤2.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 
 
+// static 멤버 데이타 : 프로그램 최초 실행시 생성자 호출
+// static 지역변수   : 함수가 최초 호출될때 생성자 호출
+//					  함수가 호출되지 않으면 생성자 호출 안됨
+
 class Cursor
 {
 private:
@@ -15,12 +19,12 @@ private:
 
 	// 유일한 객체가 static 멤버 데이타인 모델.
 //	Cursor instance; // error
-	static Cursor instance; // ok
+//	static Cursor instance; // ok
 
 public:
 	static Cursor& get_instance()
 	{
-//		static Cursor instance;
+		static Cursor instance;
 		return instance;
 	}
 };
