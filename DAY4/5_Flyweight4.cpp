c0009c78a6e35d2360b1f42a092cabc9a153cd7e
ecourse <smkang@codenuri.co.kr>
2023-05-18 16:33:24 +0900

    Add files via upload

--- DAY4/5_Flyweight4.cpp
+++ DAY4/5_Flyweight4.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <String>
 #include <vector>
+
 class Character
 {
 	char ch;
 
+	// MSWORD 에서 대부분의 문자는 아래 데이타는 공유 하게 됩니다.
+	// 그런데 아래 처럼 설계하면 공유되지 않습니다.
 	std::string font_name;
 	int color;
 	int size;
