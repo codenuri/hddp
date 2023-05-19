2ddc1614a29ab6a0deb69a71856bceb0b5248004
ecourse <smkang@codenuri.co.kr>
2023-05-19 13:25:16 +0900

    Add files via upload

---
+++ DAY5/5_GUI4.cpp
@@ -0,0 +1,50 @@
+#define USING_GUI
+#include "cppmaster.h"
+
+#include <map>
+
+class Window;
+
+// this 를 자료구조에 보관했다 사용하는 기술 - 아주 유명한 기술입니다.
+std::map<int, Window*> this_map;
+
+
+
+class Window
+{
+	int handle;
+public:
+	void Create(const char* title)
+	{
+		handle = ec_make_window(&MsgProc, title);
+
+		// "윈도우 번호 - 객체" 를 자료구조에 보관
+		this_map[handle] = this;
+	}
+
+	static int MsgProc(int hwnd, int msg, int a, int b)
+	{
+		Window* self = this_map[hwnd];
+
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN: self->LButtonDown(); break; 
+		case WM_KEYDOWN:     self->KeyDown(); break;
+		}
+		return 0;
+	}
+
+	virtual void LButtonDown() {}
+	virtual void KeyDown() {}
+};
+
+
+
+int main()
+{
+	Window w;
+	w.Create("A");
+
+	ec_process_message();
+}
+
