f97a1204f09c29b46349c3381b7f497e94d99976
ecourse <smkang@codenuri.co.kr>
2023-05-19 15:46:30 +0900

    Add files via upload

--- DAY5/QA.cpp
+++ DAY5/QA.cpp
@@ -4,10 +4,15 @@ class Array
 {
 public:
 	int arr[5];
+
+	int size() { return 5; }
+
+	int& operator[](int idx) { return arr[idx]; }
 };
 
 int main()
 {
 	Array a1 = { 1,2,3,4,5 };
 	
+	a1[0] = 10; // 
 }
