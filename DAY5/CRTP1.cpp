20061c0ae0436598b99e03e52036c6ab94a8e9c5
ecourse <smkang@codenuri.co.kr>
2023-05-19 16:12:33 +0900

    Add files via upload

--- DAY5/CRTP1.cpp
+++ DAY5/CRTP1.cpp
@@ -7,22 +7,37 @@ class Window;
 
 std::map<int, Window*> this_map;
 
-
+// 클래스 템플릿 만들때
+// T를 사용하지 않은 모든 멤버 함수는
+// 템플릿이 아닌 기반 클래스를 만들어서 제공해라
+// => "thin template" 기술
 
 class Window
 {
+protected:
 	int handle;
+public:
+	// 수백개의 윈도우 관련 함수들..
+	void MoveWindow() {} 
+
+	void LButtonDown() {}
+	void KeyDown() {}
+};
+
+template<typename T> 
+class MsgWindow : public Window
+{
 public:
 	void Create(const char* title)
 	{
 		handle = ec_make_window(&MsgProc, title);
-
 		this_map[handle] = this;
 	}
-
 	static int MsgProc(int hwnd, int msg, int a, int b)
 	{
-		Window* self = this_map[hwnd];
+//		Window* self = this_map[hwnd];
+
+		T* self = static_cast<T*>(this_map[hwnd]);
 
 		switch (msg)
 		{
@@ -32,11 +47,9 @@ public:
 		return 0;
 	}
 
-	virtual void LButtonDown() {}
-	virtual void KeyDown() {}
 };
 //----------------------------------
-class MainWindow : public Window
+class MainWindow : public MsgWindow<MainWindow>
 {
 public:
 	void LButtonDown()
